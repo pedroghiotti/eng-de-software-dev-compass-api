38e94e5e87bf2295149559a29212360d361d3b10
pedroghiotti <pedro.ghiotti.m@gmail.com>
2025-06-07 19:45:04 -0300

    fix: mensagem de notificação reformatada

--- src/main/java/br/facens/eng_de_software/dev_compass_api/notifications/model/EmailNotificationGateway.java
+++ src/main/java/br/facens/eng_de_software/dev_compass_api/notifications/model/EmailNotificationGateway.java
@@ -5,7 +5,7 @@ import br.facens.eng_de_software.dev_compass_api.model.Candidate;
 public class EmailNotificationGateway extends NotificationGateway {
     @Override
     public void sendNotification(Candidate candidate, String message) {
-        System.out.println("Usuário " + candidate.getUsername() + " notificado via email da vaga: " + message);
+        System.out.println("\n\nUsuário " + candidate.getUsername() + " notificado via email da vaga:\n" + message + "\n\n");
     }
 
 }
